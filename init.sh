b974ba15bb37ffc71a098f7049405e2365572ffc
ZhiFeng Hu <hufeng1987@gmail.com>
2017-08-12 05:12:51 +0000

    Update repository

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+rm -rf ~/.kube
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml 
+kubectl apply -f nginx.yml
